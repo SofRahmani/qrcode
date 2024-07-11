2245ddfcdabf42f40cd1f0a1b2af2d6a70698c93
SofRahmani <sofiane.rahmani12@gmail.com>
2024-07-11 21:35:49 +0200

    build: :tada: Setup project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { fontSans } from "../styles/fonts";
+import "../styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="fr">
+      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
